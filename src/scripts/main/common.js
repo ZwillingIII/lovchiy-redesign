23b0ef7ab0521feafe03c18aac52e36bcb536ad7
ZwillingIII <kirill.knyazev.20@mail.ru>
2024-04-06 16:04:19 +0300

    menu catalog mobil & main page mobil

--- src/scripts/main/common.js
+++ src/scripts/main/common.js
@@ -13,8 +13,18 @@ document.addEventListener('DOMContentLoaded', function() {
 
   const sections = new Swiper('.main-sections__items', {
     direction: 'horizontal',
-    slidesPerView: 6,
-    spaceBetween: 20,
+    slidesPerView: 2,
+    spaceBetween: 5,
+    breakpoints: {
+      580: {
+        slidesPerView: 3,
+        spaceBetween: 10
+      },
+      1000: {
+        slidesPerView: 6,
+        spaceBetween: 20,
+      },
+    },
     navigation: {
       nextEl: '.main-sections__next',
       prevEl: '.main-sections__prev'
@@ -23,8 +33,18 @@ document.addEventListener('DOMContentLoaded', function() {
 
   const newsProducts = new Swiper('.news-products__items', {
     direction: 'horizontal',
-    slidesPerView: 5,
-    spaceBetween: 20,
+    slidesPerView: 2,
+    spaceBetween: 5,
+    breakpoints: {
+      580: {
+        slidesPerView: 3,
+        spaceBetween: 10
+      },
+      1000: {
+        slidesPerView: 5,
+        spaceBetween: 20,
+      },
+    },
     navigation: {
       nextEl: '.news-products__next',
       prevEl: '.news-products__prev'
@@ -33,8 +53,18 @@ document.addEventListener('DOMContentLoaded', function() {
 
   const stocks = new Swiper('.stocks-items', {
     direction: 'horizontal',
-    slidesPerView: 4,
-    spaceBetween: 20,
+    slidesPerView: 2,
+    spaceBetween: 5,
+    breakpoints: {
+      580: {
+        slidesPerView: 3,
+        spaceBetween: 10,
+      },
+      1000: {
+        slidesPerView: 4,
+        spaceBetween: 20,
+      },
+    },
     navigation: {
       nextEl: '.stocks-navs__next',
       prevEl: '.stocks-navs__prev'
@@ -43,8 +73,18 @@ document.addEventListener('DOMContentLoaded', function() {
 
   const news = new Swiper('.news-items', {
     direction: 'horizontal',
-    slidesPerView: 4,
-    spaceBetween: 20,
+    slidesPerView: 2,
+    spaceBetween: 5,
+    breakpoints: {
+      580: {
+        slidesPerView: 3,
+        spaceBetween: 10,
+      },
+      1000: {
+        slidesPerView: 4,
+        spaceBetween: 20,
+      },
+    },
     navigation: {
       nextEl: '.news-navs__next',
       prevEl: '.news-navs__prev'
@@ -75,16 +115,59 @@ document.addEventListener('DOMContentLoaded', function() {
   $(document).on('click', '#open-catalog-menu', function () {
     const btn = $(this);
     const active = btn.hasClass('active');
+    const menu = $('.menu-catalog');
 
     if (active) {
       btn.removeClass('active');
+      menu.removeClass('active');
+      $('body').css('overflow', 'auto');
     } else {
       btn.addClass('active');
+      menu.addClass('active');
+      $('body').css('overflow', 'hidden');
     }
   });
 
   $(".header-top__menu-link .drop-list").hide();
   $(".header-top__menu-link.drop").hover(function(){
     $(this).find('.drop-list').stop().fadeToggle(300);
+  });
+
+  $(document).on('click', '.menu-catalog__section', function() {
+    const id = $(this).attr('data-id');
+    const page = $('.menu-catalog__page[data-id='+ id +']');
+
+    $('.menu-catalog__section.active').removeClass('active');
+    $('.menu-catalog__page.active').removeClass('active');
+    $(this).addClass('active');
+    page.addClass('active');
+  });
+
+  $(document).on('click', '#open-mobil-menu', function() {
+    const btn = $(this);
+    const menu = $('.mobil-menu');
+
+    if (btn.hasClass('active')) {
+      btn.removeClass('active');
+      menu.removeClass('active');
+      $('body').css('overflow', 'auto');
+    } else {
+      btn.addClass('active');
+      menu.addClass('active');
+      $('body').css('overflow', 'hidden');
+    }
+  });
+
+  $(document).on('click', '.mobil-menu__item.internal, .mobil-menu__internal-item_section.internal', function () {
+    const code = $(this).attr('data-open-id');
+    const internal = $('.mobil-menu__internal-item[data-open-id=' + code + ']');
+
+    internal.addClass('active');
+  });
+
+  $(document).on('click', '.mobil-menu__internal-item_title img', function() {
+    const code = $(this).attr('data-open-id');
+
+    $('.mobil-menu__internal-item[data-open-id=' + code + ']').removeClass('active');
   });
 })
